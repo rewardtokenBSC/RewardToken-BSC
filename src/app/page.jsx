3afa3c3380a6d8a16d47881faf118e35278fd064
ayanda-boluwatife <ayandastephen1000@gmail.com>
2024-02-24 14:03:49 +0100

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,28 @@
 "use client";
-
-import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { TbBrandTelegram, TbBrandX } from "react-icons/tb";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState(
-    "0x048ab6dbfa7444de1d77a4970b6ed19d7495db36"
-  );
   const [isCopied, setIsCopied] = useState(false);
+  const textToCopyRef = useRef(null);
 
   const copyToClipboard = () => {
-    // Create a temporary input element
-    const tempInput = document.createElement("input");
-    tempInput.value = inputValue;
-    document.body.appendChild(tempInput);
+    const textToCopy = textToCopyRef.current.innerText;
 
-    // Select the input value
-    tempInput.select();
-    tempInput.setSelectionRange(0, 99999); // For mobile devices
+    // Create a temporary textarea element
+    const tempTextArea = document.createElement("textarea");
+    tempTextArea.value = textToCopy;
+    document.body.appendChild(tempTextArea);
+
+    // Select the textarea value
+    tempTextArea.select();
+    tempTextArea.setSelectionRange(0, 99999); // For mobile devices
 
     // Copy the selected text
     document.execCommand("copy");
 
-    // Remove the temporary input
-    document.body.removeChild(tempInput);
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextArea);
 
     // Set the button text to "Copied" and reset after 3 seconds
     setIsCopied(true);
@@ -40,22 +38,17 @@ export default function Home() {
 
       {/* Navbar */}
       <div className="flex items-center z-[1000] justify-between md:px-20 px-8 py-2 bg-black bg-opacity-70 backdrop-filter backdrop-blur-lg absolute inset-x-0 top-0">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-3">
           <img src="/logo2.jpg" className="h-[50px] w-[50px]" alt="Logo" />
           <h2 className="text-white font-bold md:text-[25px] text-[14px]">
             Reward Token
           </h2>
         </div>
-        <Link
-          target="_blank"
-          href={
-            "https://pancakeswap.finance/swap?outputCurrency=0x048AB6dBfA7444De1d77A4970b6ED19d7495Db36&inputCurrency=BNB"
-          }
+        <button
+          className="bg-white py-2 md:px-10 px-6 rounded-3xl text-black font-bold md:text-[18px] text-[14px] hover:bg-transparent border hover:border-white hover:text-white"
         >
-          <button className="bg-white py-2 md:px-10 px-6 rounded-3xl text-black font-bold md:text-[18px] text-[14px] hover:bg-transparent border hover:border-white hover:text-white">
-            Buy
-          </button>
-        </Link>
+          BUY
+        </button>
       </div>
 
       <div className="relative flex flex-col md:pt-10 pt-20 gap-8 justify-center w-screen items-center h-screen">
@@ -63,23 +56,22 @@ export default function Home() {
           $RWD
         </h2>
         <p className="text-white text-opacity-70 text-[20px] font-[400] lg:w-[45%] md:w-[70%] w-[85%] text-center">
-        The most rewarding token on the Binance Smart Chain.
+          The most rewarding token on the Binance Smart Chain.
         </p>
 
         <div className="flex z-[1000] md:flex-row flex-col items-center gap-4">
-          <input
-            type="text"
-            disabled
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+          <div
+            ref={textToCopyRef}
             className="py-3 bg-black text-white rounded-3xl text-center text-[14px] md:w-[410px] w-[400px]"
-          />
-          <button
-            onClick={copyToClipboard}
-            className="bg-white py-2 px-10 rounded-3xl text-black font-medium hover:bg-transparent border hover:border-white hover:text-white"
           >
-            {isCopied ? "Copied" : "Copy"}
-          </button>
+            0x048ab6dbfa7444de1d77a4970b6ed19d7495db36
+          </div>
+          <button
+          onClick={copyToClipboard}
+          className="bg-white py-2 md:px-10 px-6 rounded-3xl text-black text-[14px] hover:bg-transparent border hover:border-white hover:text-white"
+        >
+          {isCopied ? "Copied" : "Copy"}
+        </button>
         </div>
 
         <div className="flex md:flex-row flex-col  items-center gap-5">
@@ -91,16 +83,16 @@ export default function Home() {
         </div>
 
         <div className="flex items-center gap-4">
-          <Link target="_blank" href={"http://x.com/rewardtokenBSC_"}>
+          <a target="_blank" href={"http://x.com/rewardtokenBSC_"}>
             <div className="bg-white bg-opacity-10 p-4 rounded-md text-white text-opacity-70 text-[25px]">
               <TbBrandX />
             </div>
-          </Link>
-          <Link target="_blank" href={"https://t.me/rewardtokenBSC"}>
+          </a>
+          <a target="_blank" href={"https://t.me/rewardtokenBSC"}>
             <div className="bg-white bg-opacity-10 p-4 rounded-md text-white text-opacity-70 text-[25px]">
               <TbBrandTelegram />
             </div>
-          </Link>
+          </a>
         </div>
       </div>
     </div>
